78312df8d3c33ddef84518ec24b8955feff8f416
Administrator <Administrator@ith-ml35.local>
2025-06-06 09:50:42 +0200

    Modul 04 LastLogon

--- Modul04/modul04.ps1
+++ Modul04/modul04.ps1
@@ -6,11 +6,26 @@ $days=5
 $ExpirationDate= (Get-Date).AddDays($days);
 $userprefix="User"
 $targetpath="OU=student,OU=Users,OU=ITH,DC=ith-ml35,DC=local"
-$pass="kennw0rt" | ConvertTo-SecureString -AsPlainText -Force
+$pass="Pa55w.rd" | ConvertTo-SecureString -AsPlainText -Force
 $Number=1..$usercount
 
 foreach ($z in $Number)
 {
     Write-Host $userprefix$z
-    New-ADUser -Name $userprefix$z -Path $targetpath -Enabled $true -ChangePasswordAtLogon $true -AccountPassword $pass -AccountExpirationDate $ExpirationDate
+    New-ADUser -Name $userprefix$z -Path $targetpath -Enabled $false -ChangePasswordAtLogon $true -AccountPassword $pass -AccountExpirationDate $ExpirationDate
 }
+
+# Objekte löschen in bestimmten Bereichen
+Get-ADUser -Filter	* -SearchBase $targetpath
+
+$user =Get-ADObject -Filter "ObjectClass -eq 'user'" -SearchBase $targetpath
+$user| Remove-ADObject -Confirm:$true
+
+### Suche in der AD
+Search-ADAccount -UsersOnly -AccountInactive -SearchBase $targetpath
+## Deaktivierte User löschen
+$user =Search-ADAccount -UsersOnly -AccountInactive -SearchBase $targetpath
+$user| Remove-ADObject -Confirm:$true
+
+### Nach inaktivität suchen
+Search-ADAccount -UsersOnly -AccountDisabled  | Get-ADUser | Sort-Object LastLogOnDate | Select-Object Name,LastLogOnDate,Title,DistinguishedName
